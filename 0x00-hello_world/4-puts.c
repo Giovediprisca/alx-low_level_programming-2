d00f7e71fc1e42b9ba3738951b73ac0e7e32553d
giovediprisca <chisomanene1999@gmail.com>
2022-09-09 01:20:27 -0700

    lowest 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
- #include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 /**
- * main - main block
- * return: 0
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+			puts("\"Programming is like building a multilingual puzzle");
+						return (0);
+}
